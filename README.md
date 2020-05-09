aeb50bad30d13dc25950458347b5707a941703ec
Chance Cardona <ccardona@mymail.mines.edu>
2020-05-09 14:33:37 -0600

    Feat: Implemented scanner coordinate frame to ackermann transition function.

--- README.md
+++ README.md
@@ -11,7 +11,13 @@ Code heavily inspired and using code from https://github.com/patrick--/SLAM-Expl
     <li> ./UnitA/plot_motor_ticks (or whatever one you want) </li>
 </ul>
 
-### Troubleshooting ###
+### Installation Troubleshooting ###
 If "file not found", be wary of where you're running the code from! This is only tested running code from "build" currently.  
 If there are errors with numpy/arrayobject.h first make sure python3, python3-numpy, and python3-matplotlib are installed on your system.  
 If there are errors with matplotlib module not found, you don't have matplotlib installed or it's set to the wrong path. Make sure your system's default is python 3! If it's 2, an easier fix may be to change the CMakeLists.txt file in utils/ to use python2.  
+
+### Unit A ###
+<ul>
+    <li> plot_motor_ticks.cpp Plots raw motor encoder data </li>
+    <li> filter_motor_question.cpp Plots the robot movements in 2D using the motor sensor data and Ackermann motor equations. </li>
+</ul>
--- UnitA/filter_motor_question.cpp
+++ UnitA/filter_motor_question.cpp
@@ -3,10 +3,50 @@
 
 const auto pi =  3.141592653589;
 
-//TODO: Change vectors to ROS::Pose2D.
-Pose2D filter_step(Pose2D old_pose, MotorTickRecord motor_tick, double ticks_to_mm,
-        double robot_width){
+void print_pose(Pose2D);
+Pose2D filter_step(Pose2D, MotorTickRecord, double, double, double);
+
+int main(){
     
+    namespace plt = matplotlibcpp;
+
+    const auto ticks_to_mm = 0.349;
+    const auto robot_width = 150.0; //mm
+    const auto scanner_displacement = 30.0; //mm
+
+    LegoRobot l;
+    l.read("../UnitA/robot4_motors.txt");
+    auto motor_tick_diffs = l.getMotorTicks();
+
+    vector<double> x_coords;
+    vector<double> y_coords;
+    Pose2D pose = {0.0, 0.0, 0.0};
+    
+    for (const auto& tick : motor_tick_diffs){
+        pose = filter_step(pose, tick, ticks_to_mm, robot_width, scanner_displacement);
+        print_pose(pose);
+        x_coords.push_back(get<0>(pose));
+        y_coords.push_back(get<1>(pose));
+    }
+
+
+    plt::plot(x_coords, y_coords, "bo");
+    plt::show();
+}
+
+void print_pose(Pose2D pose) {
+    const auto [x, y, theta] = pose;
+    std::cout << "(" << x << "," << y << "," << theta << ")" << std::endl;
+}
+
+//Change vectors to ROS::Pose2D.
+Pose2D filter_step(Pose2D old_pose, MotorTickRecord motor_tick, double ticks_to_mm,
+        double robot_width, double scanner_displacement){
+    //Uses Ackermann Transition Function from center of robot.
+    //Also must keep track of scanner displacement from
+    //robot center since measurements are from scanner, not center. 
+
+
     auto [left_tick, right_tick] = motor_tick;
     auto [old_x, old_y, old_theta] = old_pose;
 
@@ -21,38 +61,18 @@ Pose2D filter_step(Pose2D old_pose, MotorTickRecord motor_tick, double ticks_to_
     else {
        double alpha = ticks_to_mm * (right_tick - left_tick) / robot_width;
        double R = ticks_to_mm * left_tick / alpha;
+       //Get old_x in terms of center of robot
+       old_x = old_x - scanner_displacement * cos(old_theta);
+       old_y = old_y - scanner_displacement * sin(old_theta);
        double center_x = old_x - (R + robot_width/2) * sin(old_theta);
        double center_y = old_y - (R + robot_width/2) * -1 *cos(old_theta);
        double new_theta = fmod((old_theta + alpha), (2*pi));
-       double new_x = center_x - (R + robot_width/2) * sin(new_theta);
-       double new_y = center_y - (R + robot_width/2) * -1 *cos(new_theta);
+       double new_x = center_x + (R + robot_width/2) * sin(new_theta);
+       double new_y = center_y + (R + robot_width/2) * -1 *cos(new_theta);
+       //Get new_x back in terms of sensor
+       new_x = new_x + scanner_displacement * cos(new_theta);
+       new_y = new_y + scanner_displacement * sin(new_theta);
        return {new_x, new_y, new_theta};
     }
 
-}
-
-int main(){
-    
-    namespace plt = matplotlibcpp;
-
-    const auto ticks_to_mm = 0.349;
-    const auto robot_width = 150.0;
-
-    LegoRobot l;
-    l.read("../UnitA/robot4_motors.txt");
-    auto motor_tick_diffs = l.getMotorTicks();
-
-    vector<double> x_coords;
-    vector<double> y_coords;
-    Pose2D pose = {0.0, 0.0, 0.0};
-    
-    for (const auto& tick : motor_tick_diffs){
-        pose = filter_step(pose, tick, ticks_to_mm, robot_width);
-        x_coords.push_back(get<0>(pose));
-        y_coords.push_back(get<1>(pose));
-    }
-
-
-    plt::plot(x_coords, y_coords, "bo");
-    plt::show();
 }
